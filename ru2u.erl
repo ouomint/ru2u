0c9c7881c5592d14edd8ca1db8405943776227b7
c: <themtz@users.noreply.github.com>
2014-09-19 01:37:55 +0900

    Update ru2u.erl

--- ru2u.erl
+++ ru2u.erl
@@ -1,7 +1,7 @@
 %% coding: utf-8
 % ru2u, Resolver U2U (Username / UUID) by tHEMtZ
 -module(ru2u).
--export([n2u/1, u2n/1]).
+-export([n2u/1, u2n/1, uf/1]).
 
 n2u(Name) -> % Username to UUID
 	application:start(inets),
@@ -19,3 +19,10 @@ u2n(Uuid) -> % UUID to Username
 	application:start(ssl),
 	[{_, {_, [_, _, _, _, _, _, _], RES}}] = [httpc:request(get, {"https://sessionserver.mojang.com/session/minecraft/profile/"++Uuid, []}, [{ssl, [{verify, 0}]}], [])],
 	string:sub_word(string:substr(RES, 50, 16), 1, $\").
+uf(Tbsuuid) -> % Format UUID
+	CountHyphen = string:words(Tbsuuid, $-)-1,
+	if CountHyphen<1 ->
+		string:substr(Tbsuuid, 1, 8)++"-"++string:substr(Tbsuuid, 9, 4)++"-"++string:substr(Tbsuuid, 13, 4)++"-"++string:substr(Tbsuuid, 17, 4)++"-"++string:substr(Tbsuuid, 21);
+	true ->
+		string:join(string:tokens(Tbsuuid, "-"), "")
+	end.
