6fb0be6c55d5afdabb87cc72a64fb7a8f197377b
c: <themtz@users.noreply.github.com>
2014-09-19 19:07:40 +0900

    Update ru2u.erl

--- ru2u.erl
+++ ru2u.erl
@@ -17,11 +17,11 @@ u2n(Uuid) -> % UUID to Username
 	application:start(asn1),
 	application:start(public_key),
 	application:start(ssl),
-	[{_, {_, [_, _, _, _, _, _, _], Result}}] = [httpc:request(get, {"https://sessionserver.mojang.com/session/minecraft/profile/"++Uuid, []}, [{ssl, [{verify, 0}]}], [])],
+	[{_, {_, [_, _, _, _, _, _, _], Result}}] = [httpc:request(get, {"https://sessionserver.mojang.com/session/minecraft/profile/"++string:join(string:tokens(Uuid, "-"), ""), []}, [{ssl, [{verify, 0}]}], [])],
 	string:sub_word(string:substr(Result, 50, 16), 1, $\").
 uf(Tbsuuid) -> % Format UUID
-	CountHyphen = string:words(Tbsuuid, $-)-1,
-	if CountHyphen<1 ->
+	CountHyphen = string:words(Tbsuuid, $-),
+	if CountHyphen<2 ->
 		string:substr(Tbsuuid, 1, 8)++"-"++string:substr(Tbsuuid, 9, 4)++"-"++string:substr(Tbsuuid, 13, 4)++"-"++string:substr(Tbsuuid, 17, 4)++"-"++string:substr(Tbsuuid, 21);
 	true ->
 		string:join(string:tokens(Tbsuuid, "-"), "")
